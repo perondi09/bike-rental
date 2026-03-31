14df939b5e1da8377c9aa2c997e9473a8d277a0e
perondi <guilhermeperondi09@gmail.com>
2026-03-30 23:35:02 -0300

    feat: first commit

---
+++ src/test/java/perondi/bike_rental/BikeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package perondi.bike_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
