343ab42e62f00439b6adcedfd8a68aa20e39f807
perondi <guilhermeperondi09@gmail.com>
2026-04-13 16:33:09 -0300

    feat: create swagger configuration

---
+++ src/main/java/perondi/bike_rental/configurations/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package perondi.bike_rental.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bike rental API")
+                        .version("1.0.0")
+                        .description("API for bike rental"));
+    }
+}
